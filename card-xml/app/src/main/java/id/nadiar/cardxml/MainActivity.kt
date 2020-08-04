472ddd591ec9cb873ae5698af32c32f8d46f37b1
Nadiar AS <codxse@gmail.com>
2020-08-04 23:28:48 +0700

    android xml

---
+++ card-xml/app/src/main/java/id/nadiar/cardxml/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.nadiar.cardxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.nadiar.cardxml.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
