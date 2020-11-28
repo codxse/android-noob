2df46b94feb2c9ad3a3fe0c3d15d75927fb77b8d
Nadiar Syaripul <codxse@gmail.com>
2020-11-29 05:41:37 +0700

    binding witn findById

--- AboutMe/app/src/main/java/id/nadiar/android/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/id/nadiar/android/aboutme/MainActivity.kt
@@ -1,11 +1,32 @@
 package id.nadiar.android.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+    }
+
+    fun addNickname(addButton: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameText = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameText.text = editText.text
+        editText.visibility = View.GONE
+        addButton.visibility = View.GONE
+        nickNameText.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(addButton.windowToken, 0)
     }
 }
