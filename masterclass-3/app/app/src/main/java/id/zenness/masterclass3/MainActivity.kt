837232f9ff547f65d51f66d12d66e08f9935bd9d
Nadiar AS <codxse@gmail.com>
2021-05-24 21:20:17 +0700

    day1 - hello world

---
+++ masterclass-3/app/app/src/main/java/id/zenness/masterclass3/MainActivity.kt
@@ -0,0 +1,11 @@
+package id.zenness.masterclass3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
