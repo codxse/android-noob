661b96106682c214f44651e3dd8b5b3467d72cb8
Nadiar AS <codxse@gmail.com>
2021-11-15 06:56:57 +0700

    saved

--- dicoding/MyFlexibleFragment/app/src/main/java/id/zenness/myflexiblefragment/CategoryFragment.kt
+++ dicoding/MyFlexibleFragment/app/src/main/java/id/zenness/myflexiblefragment/CategoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 
 class CategoryFragment : Fragment(), View.OnClickListener {
     override fun onCreateView(
@@ -24,7 +25,24 @@ class CategoryFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         if (v?.id == R.id.btn_detail_category) {
-            // TODO : onclick
+            val mDetailCategoryFragment = DetailCategoryFragment()
+            val mBundle = Bundle()
+            mBundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle 20")
+            val description = "Kategori ini akan berisi produk-produk lifestyle dong"
+
+            mDetailCategoryFragment.arguments = mBundle
+            mDetailCategoryFragment.description = description
+
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(
+                    R.id.frame_container,
+                    mDetailCategoryFragment,
+                    DetailCategoryFragment::class.java.simpleName
+                )
+                addToBackStack(null)
+                commit()
+            }
         }
     }
 }
