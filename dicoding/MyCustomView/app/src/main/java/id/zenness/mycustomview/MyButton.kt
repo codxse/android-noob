9bdbc848f587ab5cdd7a3ca2974d93da1f692054
Nadiar AS <codxse@gmail.com>
2021-11-17 07:06:32 +0700

    adapter

---
+++ dicoding/MyCustomView/app/src/main/java/id/zenness/mycustomview/MyButton.kt
@@ -0,0 +1,4 @@
+package id.zenness.mycustomview
+
+class MyButton {
+}
---
+++ dicoding/MyRecyclerView/app/src/main/java/id/zenness/myrecyclerview/Hero.kt
@@ -0,0 +1,11 @@
+package id.zenness.myrecyclerview
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: Int,
+) : Parcelable
